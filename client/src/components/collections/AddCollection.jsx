3371be11aec88ba65c7baf0e7afd767339a7d068
Veronica Stork <zealous.anemone@gmail.com>
2020-07-01 23:15:23 -0400

    Add functionality for adding more than one link when creating collection

--- client/src/components/collections/AddCollection.jsx
+++ client/src/components/collections/AddCollection.jsx
@@ -3,18 +3,44 @@ import React, { useState } from 'react';
 import './AddCollection.css';
 
 const AddCollection = () => {
+  const linkField = { link: '' };
   const [author, setAuthor] = useState('');
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
-  const [hidden, setHidden] = useState('');
+  const [hidden, setHidden] = useState(false);
   const [contributors, setContributors] = useState('');
   const [text, setText] = useState('');
   const [category, setCategory] = useState('');
   const [tags, setTags] = useState('');
   const [links, setLinks] = useState('');
+  // this is for link input fields
+  const [linkInput, setLinkInput] = useState([{ ...linkField }]);
+  let tempLinks = '';
+
+  // Add new link field to form so you can add multiple links
+  const addLinkField = () => {
+    setLinkInput([...linkInput, { ...linkField }]);
+  };
+
+  // Goes through all links in different link fields and puts them together into one string
+  const addLinks = () => {
+    // Go through the link inputs (minus 1)
+    for (let i = 0; i < linkInput.length - 1; i++) {
+      // Add value of input field (plus a comma and space) to temp variable
+      tempLinks += document.getElementById(`link-${i}`).value;
+      tempLinks += ', ';
+    }
+    // Add last link (doesn't need comma after it)
+    tempLinks += document.getElementById(`link-${linkInput.length - 1}`).value;
+    // Set links state with the new temporary variable string
+    setLinks(tempLinks);
+  };
+
   const addCollection = (e) => {
     e.preventDefault();
-
+    // Supposed to put links together before fetch request - Couldn't get async to work for this
+    // Needs work. new state does not seem to be available when fetch request is made
+    addLinks();
     fetch('/api/collections', {
       method: 'POST',
       headers: {
@@ -35,6 +61,7 @@ const AddCollection = () => {
       .then((response) => response.json())
       .then((data) => {
         console.log('Response: ', data);
+        console.log('Links: ', links);
       })
       .catch((err) => {
         console.log('Error on add collection', err);
@@ -128,15 +155,20 @@ const AddCollection = () => {
           />
         </label>
         <br />
-        <label>
-          Links:
-          <input
-            type="text"
-            name="links"
-            onChange={(e) => setLinks(e.target.value)}
-            value={links}
-          />
-        </label>
+        {linkInput.map((val, idx) => {
+          const link = `link-${idx}`;
+          return (
+            <div key={`link-${idx}`}>
+              <label>
+                Link:
+                <input type="text" name={link} id={link} />
+              </label>
+            </div>
+          );
+        })}
+        <button type="button" id="add-link" onClick={addLinkField}>
+          +
+        </button>
         <br />
         <button type="submit" onClick={addCollection}>
           Submit
