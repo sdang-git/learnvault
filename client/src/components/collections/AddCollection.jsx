9a44f90ecf805f83dca9cd20cfa9b4c8d1a71624
Veronica Stork <zealous.anemone@gmail.com>
2020-07-05 20:20:45 -0400

    Redirect to AllCollections upon successful adding of collection

--- client/src/components/collections/AddCollection.jsx
+++ client/src/components/collections/AddCollection.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 
 import './AddCollection.css';
 
 const AddCollection = () => {
   const linkField = { link: '' };
+
+  const [redirect, setRedirect] = useState(false);
   const [formData, setFormData] = useState({
     author: '',
     title: '',
@@ -30,23 +33,13 @@ const AddCollection = () => {
         ...formData,
         hidden: !hidden,
       });
-    } else if (e.target.name === 'link') {
-      console.log('links state: ', links);
-      let temp;
-      e.target.id === 'link-0'
-        ? (temp = `${e.target.value}`)
-        : (temp = `${links}, ${e.target.value}`);
-      console.log('temp: ', temp);
-      setFormData({
-        ...formData,
-        links: temp,
-      });
     } else {
       setFormData({
         ...formData,
         [e.target.name]: e.target.value,
       });
     }
+    console.log(`In updateForm. formData is: ${JSON.stringify(formData)}`);
   };
 
   const {
@@ -63,14 +56,14 @@ const AddCollection = () => {
 
   const addCollection = (e) => {
     e.preventDefault();
-    // const temp = [];
-    // Object.keys(formData).forEach((key) => {
-    //   if (key.indexOf('link') > -1 && key !== 'links') {
-    //     temp.push(formData[key]);
-    //     delete formData[key];
-    //   }
-    //   formData.links = String(temp);
-    // });
+    const temp = [];
+    Object.keys(formData).forEach((key) => {
+      if (key.indexOf('link') > -1 && key !== 'links') {
+        temp.push(formData[key]);
+        delete formData[key];
+      }
+      formData.links = String(temp);
+    });
     fetch('/api/collections', {
       method: 'POST',
       headers: {
@@ -80,7 +73,10 @@ const AddCollection = () => {
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log('Response: ', data);
+        if (data) {
+          console.log('AddCollection successful!');
+          setRedirect(true);
+        }
       })
       .catch((err) => {
         console.log('Error on add collection', err);
@@ -88,134 +84,128 @@ const AddCollection = () => {
   };
 
   return (
-    <div className="addCollection">
-      <h1>Add New Collection</h1>
-      <form>
-        <label>
-          Author:
-          <span className="label-note">(Required)</span>
-          <input
-            type="text"
-            name="author"
-            required
-            onChange={(e) => updateForm(e)}
-            value={author}
-          />
-        </label>
-        <br />
-        <label>
-          Title:
-          <span className="label-note">(Required)</span>
-          <input
-            type="text"
-            name="title"
-            required
-            onChange={(e) => updateForm(e)}
-            value={title}
-          />
-        </label>
-        <br />
-        <label>
-          Description:
-          <span className="label-note">(Required)</span>
-          <input
-            type="text"
-            name="description"
-            required
-            onChange={(e) => updateForm(e)}
-            value={description}
-          />
-        </label>
-        <br />
-        <label>
-          Hidden:
-          <input
-            type="checkbox"
-            name="hidden"
-            onChange={(e) => updateForm(e)}
-            value={hidden}
-          />
-        </label>
-        <br />
-        <label>
-          Contributors:
-          <input
-            type="text"
-            name="contributors"
-            onChange={(e) => updateForm(e)}
-            value={contributors}
-          />
-        </label>
-        <br />
-        <label>
-          Text:
-          <input
-            type="text"
-            name="text"
-            onChange={(e) => updateForm(e.target.value)}
-            value={text}
-          />
-        </label>
-        <br />
-        <label>
-          Category:
-          <input
-            type="text"
-            name="category"
-            onChange={(e) => updateForm(e)}
-            value={category}
-          />
-        </label>
-        <br />
-        <label>
-          Tags:
-          <input
-            type="text"
-            name="tags"
-            onChange={(e) => updateForm(e)}
-            value={tags}
-          />
-        </label>
-        <br />
-        {linkInput.map((val, idx) => {
-          const link = `link-${idx}`;
-          return (
-            <div key={`link-${idx}`}>
-              <label>
-                Link:
-                <input
-                  type="text"
-                  name="link"
-                  id={link}
-                  onChange={(e) => updateForm(e)}
-                />
-              </label>
-              <button
-                type="button"
-                id="add-link"
-                onClick={() => {
-                  addLinkField();
-                }}
-              >
-                +
-              </button>
-            </div>
-          );
-        })}
-        {/* <button
-          type="button"
-          id="add-link"
-          onClick={() => {
-            addLinkField();
-          }} */}
-        {/* >
-          +
-        </button> */}
-        <br />
-        <button type="submit" onClick={addCollection}>
-          Submit
-        </button>
-      </form>
+    <div>
+      {redirect ? <Redirect to="/" /> : null}
+      <div className="addCollection">
+        <h1>Add New Collection</h1>
+        <form>
+          <label>
+            Author:
+            <span className="label-note">(Required)</span>
+            <input
+              type="text"
+              name="author"
+              required
+              onChange={(e) => updateForm(e)}
+              value={author}
+            />
+          </label>
+          <br />
+          <label>
+            Title:
+            <span className="label-note">(Required)</span>
+            <input
+              type="text"
+              name="title"
+              required
+              onChange={(e) => updateForm(e)}
+              value={title}
+            />
+          </label>
+          <br />
+          <label>
+            Description:
+            <span className="label-note">(Required)</span>
+            <input
+              type="text"
+              name="description"
+              required
+              onChange={(e) => updateForm(e)}
+              value={description}
+            />
+          </label>
+          <br />
+          <label>
+            Hidden:
+            <input
+              type="checkbox"
+              name="hidden"
+              onChange={(e) => updateForm(e)}
+              value={hidden}
+            />
+          </label>
+          <br />
+          <label>
+            Contributors:
+            <input
+              type="text"
+              name="contributors"
+              onChange={(e) => updateForm(e)}
+              value={contributors}
+            />
+          </label>
+          <br />
+          <label>
+            Text:
+            <input
+              type="text"
+              name="text"
+              onChange={(e) => updateForm(e)}
+              value={text}
+            />
+          </label>
+          <br />
+          <label>
+            Category:
+            <input
+              type="text"
+              name="category"
+              onChange={(e) => updateForm(e)}
+              value={category}
+            />
+          </label>
+          <br />
+          <label>
+            Tags:
+            <input
+              type="text"
+              name="tags"
+              onChange={(e) => updateForm(e)}
+              value={tags}
+            />
+          </label>
+          <br />
+          {linkInput.map((val, idx) => {
+            const link = `link-${idx}`;
+            return (
+              <div key={`link-${idx}`}>
+                <label>
+                  Link:
+                  <input
+                    type="text"
+                    name={link}
+                    id={link}
+                    onChange={(e) => updateForm(e)}
+                  />
+                </label>
+                <button
+                  type="button"
+                  id="add-link"
+                  onClick={() => {
+                    addLinkField();
+                  }}
+                >
+                  +
+                </button>
+              </div>
+            );
+          })}
+          <br />
+          <button type="submit" onClick={addCollection}>
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
